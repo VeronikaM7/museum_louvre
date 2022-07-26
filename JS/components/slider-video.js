e36fe99b097a8d3a48e7218456f3a9f1636e8a8a
veronika_medvedeva <pirsova1996@mail.ru>
2022-07-26 15:14:54 +0300

    add slider-video

---
+++ JS/components/slider-video.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 10,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
